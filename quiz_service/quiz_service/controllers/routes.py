0a17ef53c06692822f32fe3d7546f9a5e44c8a65
Eduardo <euodeionomedeusuario@gmail.com>
2018-01-09 11:44:37 -0300

    Adicionando relacionamneto entre disciplinas e tópicos

--- quiz_service/quiz_service/controllers/routes.py
+++ quiz_service/quiz_service/controllers/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
+from bson.objectid import ObjectId
+
 
 from quiz_service import app
 from quiz_service import db
@@ -12,35 +14,80 @@ def index():
 #Retornando todas as questões do BD
 @app.route("/quiz_service/questions/")
 def get_all_questions():
-    qs = db.question.find()
+    result = db.question.find()
 
-    #Melhore esta parte do código
+    print(result)
+
     questions = []
-    for q in qs:
-        question = [
-            str(q["_id"]),
-            q["title"],
-            q["topic"],
-            q["level"]
-            ]
-        questions.append(question)
+    for item in result:
+        if item["_id"]:
+            item["_id"] = str(item["_id"])
+            item["topic"]["_id"] = str(item["topic"]["_id"])
+            item["topic"]["abstract"]["_id"] = str(item["topic"]["abstract"]["_id"])
+        questions.append(item)
 
     return jsonify(questions)
 
-#Retornando todas as questões com tópico igual a topic
-@app.route("/quiz_service/questions/<topic>", methods=["GET"])
-def get_questions_by_topic(topic):
-    qs = db.question.find({"topic" : topic})
 
-    #Melhore esta parte do código
+#Retornando todas as questões por tópico
+@app.route("/quiz_service/questions/", methods=["POST", "GET"])
+def get_questions_by_topic():
+    topic_id = request.form.get("topic_id")
+
+    result = db.question.find({"topic._id" : ObjectId(topic_id)})
+
+    print(result)
+
     questions = []
-    for q in qs:
-        question = [
-            str(q["_id"]),
-            q["title"],
-            q["topic"],
-            q["level"]
-            ]
-        questions.append(question)
+    for item in result:
+        if item["_id"]:
+            item["_id"] = str(item["_id"])
+            item["topic"]["_id"] = str(item["topic"]["_id"])
+            item["topic"]["abstract"]["_id"] = str(item["topic"]["abstract"]["_id"])
+        questions.append(item)
 
     return jsonify(questions)
+
+#Retornando todas as disciplinas no BD
+@app.route("/quiz_service/abstracts/", methods=["GET"])
+def get_all_abstracts():
+    result = db.abstract.find()
+
+    abstracts = []
+    for item in result:
+        if item["_id"]:
+            item["_id"] = str(item["_id"])
+        abstracts.append(item)
+
+    return jsonify(abstracts)
+
+
+#Retornando todas os tópicos no BD
+@app.route("/quiz_service/topics/", methods=["GET"])
+def get_all_topics():
+    result = db.topic.find()
+
+    topics = []
+    for item in result:
+        if item["_id"]:
+            item["_id"] = str(item["_id"])
+            item["abstract"]["_id"] = str(item["abstract"]["_id"])
+        topics.append(item)
+
+    return jsonify(topics)
+
+#Retornando todos os tópicos por disciplinas
+@app.route("/quiz_service/topics/", methods=["POST", "GET"])
+def get_topics_by_abstract():
+    abstract_id = request.form.get("abstract_id")
+
+    result = db.topic.find({"abstract._id" : ObjectId(abstract_id)})
+
+    topics = []
+    for item in result:
+        if item["_id"]:
+            item["_id"] = str(item["_id"])
+            item["abstract"]["_id"] = str(item["abstract"]["_id"])
+        topics.append(item)
+
+    return jsonify(topics)
--- quiz_service/quiz_service/static/js/script.js
+++ quiz_service/quiz_service/static/js/script.js
@@ -1,6 +1,27 @@
 
 $(document).ready(function() {
-  function createQuestion(title, classe, button) {
+  /*Habilitando o uso de efeitos do Materialize nos selects*/
+  $('select').material_select();
+
+  /*Criando um novo tópico*/
+  function createTopic(abstract) {
+    $topic = $("<option />").text(abstract["name"]).attr("value", abstract["_id"]);
+
+    return $topic;
+
+  }
+
+  /*Criando uma nova disciplina*/
+  function createAbstract(abstract) {
+    $abstract = $("<option />").text(abstract["name"]).attr("value", abstract["_id"]);
+
+    return $abstract;
+
+  }
+
+
+  /*Criando uma nova questão*/
+  function createQuestion(question) {
     $question = $("<div />")
                 .addClass("row")
                 .append($("<div />")
@@ -12,41 +33,77 @@ $(document).ready(function() {
                             .append($("<span />")
                                 .addClass("card-title")
                                 .attr("name", "title")
-                                .text(title)))
+                                .text(question["title"])))
                         .append($("<div />")
                             .addClass("card-action")
                             .append($("<a />")
-                                .addClass(classe)
                                 .addClass("btn-floating halfway-fab waves-effect waves-light red")
                                 .append($("<i />")
                                     .addClass("material-icons")
-                                    .text(button))))));
+                                    .text("add"))))));
 
      return $question
 }
 
-  /*Carregando questões consultadas no BD*/
-  function loadQuestions(questions) {
-    for(index in questions) {
-      $("#questions-list").append(createQuestion(questions[index][1], "btnAdd", "add"));
-    }
+  /*Carregando disciplinas disponíveis no BD*/
+  function loadAbstracts() {
+    $.ajax({
+      url: "http://127.0.0.1:5000/quiz_service/abstracts/",
+      type: "GET",
+      success: function(data) {
+        for(index in data) {
+          $("#abstract").append(createAbstract(data[index]));
+        }
+        /*Recarregando as configurações do selects*/
+        $('select').material_select();
+      }
+    });
   }
 
+  loadAbstracts();
 
-  /*Habilitando o uso de efeitos do Materialize nos selects*/
-  $('select').material_select();
-
-  /*Pesquisando uma lista de questões no BD*/
-  $("#btnSearch").click(function(event) {
-      console.log("Pesquisando uma lista de questões no BD")
-
-      $.ajax({
-        url: "http://127.0.0.1:5000/quiz_service/questions/1",
-        type: "GET",
-        success: function(data) {
-          loadQuestions(data);
+  /*Carregando os tópicos no BD*/
+  function loadTopics(abstract_id) {
+    $.ajax({
+      url: "http://127.0.0.1:5000/quiz_service/topics/",
+      type: "POST",
+      data: {abstract_id: abstract_id},
+      success: function(data) {
+        for(index in data) {
+          $("#topic").append(createTopic(data[index]));
         }
-      });
+        /*Recarregando as configurações do selects*/
+        $('select').material_select();
+      }
+    });
+  }
+
+  /*Carregando tópicos quando disciplina selecionada*/
+  $("#abstract").change(function(event) {
+    var abstract_id = $("select#abstract").val();
+    loadTopics(abstract_id);
+  });
+
+
+  /*Carregando questões consultadas no BD*/
+  function loadQuestions(topic_id) {
+    $.ajax({
+      url: "http://127.0.0.1:5000/quiz_service/questions/",
+      type: "POST",
+      data: {topic_id : topic_id},
+      success: function(data) {
+        for(index in data) {
+          $("#questions-list").append(createQuestion(data[index]));
+        }
+      }
+    });
+  }
+
+  /*Pesquisando uma lista de questões por tópico*/
+  $("#btnSearch").click(function(event) {
+    var topic_id = $("select#topic").val();
+    console.log(topic_id)
+    loadQuestions(topic_id);
   });
 
 });
